4481a8c811260f674ea34ccfb2723126dcb1cf02
Udayhrmt45 <hiremathuday68@gmail.com>
2026-03-24 11:57:39 +0530

    fix /auth/bootstrap in index.js

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,3 +1,24 @@
 const app = require("../src/app");
+const { connectDatabase } = require("../src/config/database");
+const { ensureSuperAdmin } = require("../src/services/superAdminService");
 
-module.exports = app;
+let bootstrapPromise;
+
+async function ensureBootstrap() {
+  if (!bootstrapPromise) {
+    bootstrapPromise = (async () => {
+      await connectDatabase();
+      await ensureSuperAdmin();
+    })().catch((error) => {
+      bootstrapPromise = null;
+      throw error;
+    });
+  }
+
+  return bootstrapPromise;
+}
+
+module.exports = async (req, res) => {
+  await ensureBootstrap();
+  return app(req, res);
+};
